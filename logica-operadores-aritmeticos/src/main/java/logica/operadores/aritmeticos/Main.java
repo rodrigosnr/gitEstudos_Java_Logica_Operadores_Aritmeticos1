a41f9fc16d1a6c7e450d56304a27a403c7831cd2
rodri <rodri@192.168.239.130>
2020-04-29 01:54:08 -0300

    Operadores commit

---
+++ logica-operadores-aritmeticos/src/main/java/logica/operadores/aritmeticos/Main.java
@@ -0,0 +1,63 @@
+package logica.operadores.aritmeticos;
+
+import br.com.softblue.commons.io.Console;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// DESCOMENTE CADA TRECHO DE CÓDIGO PARA UTILIZÁ-LO
+
+		/*
+		 Concatenando texto String texto1; String texto2;
+		 
+		 System.out.println("Digite um número:"); texto1 = Console.readString();
+		 
+		 System.out.println("Digite o segundo número:"); texto2 =
+		 Console.readString();
+		 
+		 String textoJunto = texto1 + texto2; System.out.println(textoJunto);
+		 */
+
+		/*
+		 Fórmula de Bhaskara System.out.println("a:"); int a = Console.readInt();
+		 
+		 System.out.println("b:"); int b = Console.readInt();
+		 
+		 System.out.println("c:"); int c = Console.readInt();
+		 
+		 double delta = Math.pow(b, 2) - 4 * a * c;
+		 
+		 double x1 = (-b + Math.sqrt(delta)) / (2 * a); double x2 = (-b -
+		 Math.sqrt(delta)) / (2 * a);
+		 
+		 System.out.println("X1 = " + x1); System.out.println("X2 = " + x2);
+		 */
+
+		/*
+		 Cálculo do perímetro e área do quadrado
+		 System.out.println("Digite a medida de um dos lados do quadrado:"); int lado
+		 = Console.readInt(); int perimetro = 4 * lado; double area = Math.pow(lado,
+		 2); System.out.println("O perímetro é: " + perimetro);
+		 System.out.println("A área é: " + area + "cm2");
+		 */
+
+		/*Mostrando se um número é par ou ímpar(usando a divisão por 2)
+		System.out.println("Digite um número: ");
+		String resultado;
+		
+		int numero = Console.readInt();
+		int resto = numero % 2;
+		
+		if (resto != 0) {
+			resultado = "Ímpar";
+		} else {
+			resultado = "Par";
+		}
+		
+		System.out.println("Na divisão do número digitado por 2 temos o resto " + resto
+				+ ". Isso significa que o número digitado é um número " + resultado);
+				
+		*/
+	}
+
+}
